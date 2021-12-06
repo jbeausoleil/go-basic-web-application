1bc0e456d9f624a8f869787d22cacede45d6717d
Justin Beausoleil <beausoleil.justin@gmail.com>
2021-12-05 17:02:38 -0700

    Add AppConfig for template cache.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/jbeausoleil/go-basic-web-application/pkg/config"
 	"github.com/jbeausoleil/go-basic-web-application/pkg/handlers"
+	"github.com/jbeausoleil/go-basic-web-application/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,6 +13,14 @@ const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	app := config.AppConfig{}
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
